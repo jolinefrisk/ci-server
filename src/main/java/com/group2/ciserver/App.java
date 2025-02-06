21a90b3c4a5b1a3502b84fbca746f3eb9b42bb93
jolinefrisk <joline_frisk@hotmail.com>
2025-02-06 11:49:40 +0100

    Initial commit for CI Server project

---
+++ src/main/java/com/group2/ciserver/App.java
@@ -0,0 +1,13 @@
+package com.group2.ciserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
